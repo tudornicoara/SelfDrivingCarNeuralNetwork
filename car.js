a081b1d96b1c4ee9d16e3cd8042df737ac30f14c
Tudor Nicoara <nicoaratudor@gmail.com>
2022-09-14 16:52:24 +0300

    Added traffic and collision with traffic

--- car.js
+++ car.js
@@ -1,5 +1,5 @@
 class Car{
-    constructor(x, y, width, height) {
+    constructor(x, y, width, height, controlType, maxSpeed = 3) {
         this.x = x;
         this.y = y;
         this.width = width;
@@ -7,26 +7,37 @@ class Car{
 
         this.speed = 0;
         this.acceleration = 0.2;
-        this.maxSpeed = 3;
+        this.maxSpeed = maxSpeed;
         this.friction = 0.05;
         this.angle = 0;
         this.damaged = false;
 
-        this.sensor = new Sensor(this);
-
-        this.controls = new Controls();
+        if (controlType !== "DUMMY") {
+            this.sensor = new Sensor(this);
+        }
+        this.controls = new Controls(controlType);
     }
 
-    update(roadBorders) {
-        this.#move();
-        this.polygon = this.#createPolygon();
-        this.damaged = this.#assessDamage(roadBorders);
-        this.sensor.update(roadBorders);
+    update(roadBorders, traffic) {
+        if (!this.damaged) {
+            this.#move();
+            this.polygon = this.#createPolygon();
+            this.damaged = this.#assessDamage(roadBorders, traffic);
+        }
+        if (this.sensor) {
+            this.sensor.update(roadBorders, traffic);
+        }
     }
 
-    #assessDamage(roadBorders) {
+    #assessDamage(roadBorders, traffic) {
         for (let i = 0; i < roadBorders.length; i++) {
             if (polysIntersect(this.polygon, roadBorders[i])) {
+                return true;
+            }
+        }
+
+        for (let i = 0; i < traffic.length; i++) {
+            if (polysIntersect(this.polygon, traffic[i].polygon)) {
                 return true;
             }
         }
@@ -86,7 +97,7 @@ class Car{
             this.speed = 0;
         }
 
-        if (this.speed != 0) {
+        if (this.speed !== 0) {
             const flip = this.speed > 0 ? 1 : -1;
 
             if (this.controls.left) {
@@ -102,11 +113,11 @@ class Car{
         this.y -= Math.cos(this.angle)*this.speed;
     }
 
-    draw(ctx) {
+    draw(ctx, color) {
         if (this.damaged) {
             ctx.fillStyle = "gray";
         } else {
-            ctx.fillStyle = "black";
+            ctx.fillStyle = color;
         }
 
         ctx.beginPath();
@@ -116,6 +127,8 @@ class Car{
         }
         ctx.fill();
 
-        this.sensor.draw(ctx);
+        if (this.sensor) {
+            this.sensor.draw(ctx);
+        }
     }
 }
--- sensor.js
+++ sensor.js
@@ -9,17 +9,20 @@ class Sensor {
         this.readings = [];
     }
 
-    update(roadBorders) {
+    update(roadBorders, traffic) {
         this.#castRays();
         this.readings = [];
         for (let i = 0; i < this.rays.length; i++) {
             this.readings.push(
-                this.#getReading(this.rays[i], roadBorders)
+                this.#getReading(
+                    this.rays[i],
+                    roadBorders,
+                    traffic)
             );
         }
     }
 
-    #getReading(ray, roadBorders) {
+    #getReading(ray, roadBorders, traffic) {
         let touches = [];
 
         for (let i = 0; i < roadBorders.length; i++) {
@@ -35,12 +38,27 @@ class Sensor {
             }
         }
 
-        if (touches.length == 0) {
+        for (let i = 0; i < traffic.length; i++) {
+            const poly = traffic[i].polygon;
+            for (let j = 0;j < poly.length; j++) {
+                const value = getIntersection(
+                    ray[0],
+                    ray[1],
+                    poly[j],
+                    poly[(j+1)%poly.length]
+                );
+                if (value) {
+                    touches.push(value);
+                }
+            }
+        }
+
+        if (touches.length === 0) {
             return null;
         } else {
             const offsets = touches.map(e => e.offset);
             const minOffset = Math.min(...offsets);
-            return touches.find(e => e.offset == minOffset);
+            return touches.find(e => e.offset === minOffset);
         }
     }
 
@@ -50,7 +68,7 @@ class Sensor {
             const rayAngle = lerp(
                 this.raySpread/2, 
                 -this.raySpread/2,
-                this.rayCount == 1 ? 0.5 : i/(this.rayCount - 1))
+                this.rayCount === 1 ? 0.5 : i/(this.rayCount - 1))
                 + this.car.angle;
 
             const start ={x: this.car.x, y: this.car.y};
